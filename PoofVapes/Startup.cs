6529892444fe307238ed8491b9540cf45f5ba473
Patrick Yee <patrickshyee@gmail.com>
2015-05-30 21:40:43 -0600

    Color scheme using LESS files

---
+++ PoofVapes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PoofVapes.Startup))]
+namespace PoofVapes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
